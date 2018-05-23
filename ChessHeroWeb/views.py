f9a94e210ed62136f9d4c824f0eeb2bf7c45df95
Centauria <centauria_v_c@163.com>
2018-05-23 13:06:01 +0800

    ChessHeroWeb OJBK!

---
+++ ChessHeroWeb/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import chess
+import chess.uci
+import chess.svg
+import json
+
+eg1 = chess.uci.popen_engine("D:\Program Files (x86)\Arena\Engines\Hermann\Hermann28_64.exe",
+							 engine_cls=chess.uci.Engine)
+eg2 = chess.uci.popen_engine("D:\Program Files (x86)\Arena\Engines\Stockfish\stockfish_8_x64.exe",
+							 engine_cls=chess.uci.Engine)
+eg1.uci()
+eg2.uci()
+
+board = chess.Board()
+
+
+# Create your views here.
+@csrf_exempt
+def showBoard(request):
+	if request.method == 'POST':
+		if request.POST.get('what') == 'start':
+			if board.turn:
+				eg1.position(board)
+				result = eg1.go(movetime=3000)
+			else:
+				eg2.position(board)
+				result = eg2.go(movetime=3000)
+			board.push(result[0])
+			return HttpResponse(chess.svg.board(board, size=600))
+	else:
+		context = {}
+		context['eg1'] = eg1.name
+		context['eg2'] = eg2.name
+		context['board'] = chess.svg.board(board, size=600)
+		return render(request, 'showboard.html', context)
+
+
+@csrf_exempt
+def start(request):
+	print(233)
+	pass
